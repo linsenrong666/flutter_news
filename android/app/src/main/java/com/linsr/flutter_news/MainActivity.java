3253671540886b35a4ec9b2fbba6c2af356bee00
linsenrong <linsenrong@jd.com>
2022-10-08 17:38:29 +0800

    Initial commit

---
+++ android/app/src/main/java/com/linsr/flutter_news/MainActivity.java
@@ -0,0 +1,6 @@
+package com.linsr.flutter_news;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _WelcomePageState();
+  }
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('welcome page'),
+        ),
+      ),
+    );
+  }
+}
